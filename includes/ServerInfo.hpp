b37ed65178c47523bdf3308fb80b6a2e4f630afe
Kevin JEBANE <kejebane@e3r3p11.clusters.42paris.fr>
2022-11-28 18:41:58 +0100

    ServerInfo additions, Location struct are now using std::string rather than char, Setter functions added, IP should be std::string, maybe, Default constructor uses arbitrary values, useless for now, Location attributes is no longer one instance, but a whole vector

--- includes/ServerInfo.hpp
+++ includes/ServerInfo.hpp
@@ -3,9 +3,9 @@
 
 struct Location
 {
-	char	*uri;
-	char	*root;
-	char	*index;
+	std::string	*uri;
+	std::string	*root;
+	std::string	*index;
 	int		allow[3]; // GET POST DELETE : 0 si interdit, 1 si autorise
 };
 
@@ -14,13 +14,15 @@ class ServerInfo {
 
 	public:
 		ServerInfo(void);
-
 		// Setters
+		void	setIp(std::string ip);
+		void	setServerName(std::string name);
+		void	setClentSize(long x);
 
 	private:
-		int		ip;
-		char	*server_name;
-		Location	_loc;
+		int		ip; //string might be better ?
+		std::string	server_name;
+		std::string::vector<Location>	_loc;
 		int			allow[3];
 		long		clientSize;
 		int			autoindex; //0 si off, 1 si on
--- srcs/ServerInfo.cpp
+++ srcs/ServerInfo.cpp
@@ -8,7 +8,24 @@ ServerInfo::ServerInfo(void)
 	// Default constructor
 	this->ip = 0;
 	this->server_name = "localhost";
-	this->_loc = proute; ////////
-	this->clientSize = NULL;
+	{
+		this->_loc.uri = "http://localhost";	//??
+		this->_loc.root = "/root/"; 		//??
+		this->_loc.index = "/index.html";	//??
+	}
+	this->clientSize = 0;
 	this->autoindex = 0; //1 ?
+}
+
+ServerInfo::setIp(std::string ip)
+{
+	// Simple setter : IP
+	this->ip = ip;
+}
+
+
+ServerInfo::setServerName(std::string name)
+{
+	// Simple setter : server_name
+	this->server_name = name;
 }
