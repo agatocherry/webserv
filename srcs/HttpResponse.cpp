a5d774a1f4f7f1842099458a12a897cd1369de11
user42 <kejebane@student.42.fr>
2023-01-02 15:26:07 +0100

    HttpResponse.cpp 80% complete, Header generation + Body Generation implemented, Response final object generation implemented, Lacking specific error status (400, 404, 405 etc) generation AND Autoindex generation (pid=fork + LS into tmp file to send, maybe), proofreading needed

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -9,19 +9,58 @@ HttpResponse::HttpResponse(std::string file, int status, int autoindex)
 	// 1st : Verify if file exists
 	// 2nd : Open it and retrieve its content into a string
 	// 3rd : DEBUG, print the content in outstream
+	//  	 if the file didn't exist, set status to 400, ERROR
+	// 4th : Generate a header + body according to situation
 	
 	int	pid;
 
 //	pid = fork();
 //	if (pid == 0)
+	(void)pid;	//Not necessary due to open?
+			//Subject to change with autoindex implementation
 	{
 		std::string	path = "../" + file;
 		std::ifstream	filestream;
 
-		filestream.open(path.c_str());
-		filestream >> this->_file_content; 
-		std::cout << this->file_content << std::endl; 	//DEBUG
-		this->_status = 200;				//SUCCESS
-		
+		int	ret = open(path.c_str());
+		if (ret < 0)
+			this->_status = 400;			//TEMPORARY 400
+		else
+		{	
+			filestream.open(path.c_str());
+			filestream >> this->_file_content; 
+			std::cout << this->file_content << std::endl; 	//DEBUG
+			this->_status = 200;				//SUCCESS
+		}
+		close(ret);
 	}
+	// Specific Error file detection 400, 401, 402, 403, 404 etc.
+	// to be determined...
+	// temporary 400 set
+	if (this->_status >= 400)
+	{
+		this->_header = "HTTP/1.1 400 ERROR\n";
+		this->_body = "Error 400 temporary\n";
+	}
+	if (this->_status >= 200 && this->_status < 300)
+	{
+		this->_header = "HTTP/1.1 200 OK\n";
+		this->_body = this->file_content;
+	}
+	this->_header = this->_header + "Content-Lenght: 1000\n";	//TEMPORARY 1000
+	// When HEADER + BODY are generated, join both to
+	// create the response
+	this->_response = this->_header;
+	this->_response = this->_response + "\n\n";
+	this->_response = this->_response + this->_body;
+}
+
+HttpResponse::~HttpResponse(void)
+{
+	return ;
+}
+
+std::string	HttpResponse::getResponse(void) const
+{
+	return (this->_response);
 }
