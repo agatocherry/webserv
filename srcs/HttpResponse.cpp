d3e261204a8444b44a39b326a7a4c3aa3b6f6d8f
user42 <kejebane@student.42.fr>
2023-01-06 12:47:16 +0100

    Re-prototyped HttpResponse constructor, removed status for now, Added the carriage return for all instances of newline in response object, Content-length now uses the clientSize as its definitive value, Specific Error not yet processed but might need ConfigInfo error_file map object

--- srcs/HttpResponse.cpp
+++ srcs/HttpResponse.cpp
@@ -3,9 +3,14 @@
 #include "webserv.hpp"
 #include "HttpResponse.hpp"
 
-HttpResponse::HttpResponse(std::string file, int status, int autoindex)
+//HttpResponse::HttpResponse(std::string file, ConfigInfo conf, int status, int autoindex)
+HttpResponse::HttpResponse(std::string file, ConfigInfo conf, int autoindex)
 {
 	// Constructor
+	// PARAM : 	file = path to file to open ("/test" for example)
+	// 		conf = param file containing the map to [error_files] + [clientSize]
+	// 		autoindex = 1 or 0, indicator of auto-generation of an index
+	// 			if a folder is set as param-1 [file-to-open]
 	// 1st : Verify if file exists
 	// 2nd : Open it and retrieve its content into a string
 	// 3rd : DEBUG, print the content in outstream
@@ -18,19 +23,20 @@ HttpResponse::HttpResponse(std::string file, int status, int autoindex)
 //	if (pid == 0)
 	(void)pid;	//Not necessary due to open?
 			//Subject to change with autoindex implementation
+	//Why is status a parameter ? Isn't it blank ?
 	{
 		std::string	path = "../" + file;
 		std::ifstream	filestream;
 
 		int	ret = open(path.c_str());
 		if (ret < 0)
-			this->_status = 400;			//TEMPORARY 400
+			this->_status = 400;			// TEMPORARY 400
 		else
 		{	
 			filestream.open(path.c_str());
 			filestream >> this->_file_content; 
-			std::cout << this->file_content << std::endl; 	//DEBUG
-			this->_status = 200;				//SUCCESS
+			std::cout << this->file_content << std::endl; 	// DEBUG
+			this->_status = 200;				// SUCCESS
 		}
 		close(ret);
 	}
@@ -39,19 +45,21 @@ HttpResponse::HttpResponse(std::string file, int status, int autoindex)
 	// temporary 400 set
 	if (this->_status >= 400)
 	{
-		this->_header = "HTTP/1.1 400 ERROR\n";
-		this->_body = "Error 400 temporary\n";
+		this->_header = "HTTP/1.1 400 ERROR\r\n";
+		this->_body = "Error 400 temporary\r\n";
 	}
 	if (this->_status >= 200 && this->_status < 300)
 	{
-		this->_header = "HTTP/1.1 200 OK\n";
+		this->_header = "HTTP/1.1 200 OK\r\n";
 		this->_body = this->file_content;
 	}
-	this->_header = this->_header + "Content-Lenght: 1000\n";	//TEMPORARY 1000
+	this->_header = this->_header + "Content-Lenght: ";	// Content-length = clientSize
+	this->_header = this->_header + ConfigInfo.size;
+	this->_header = this->_header + "\r\n";	
 	// When HEADER + BODY are generated, join both to
 	// create the response
 	this->_response = this->_header;
-	this->_response = this->_response + "\n\n";
+	this->_response = this->_response + "\r\r\n\n";
 	this->_response = this->_response + this->_body;
 }
 
